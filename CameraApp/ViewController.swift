eeec82f7cf65ea1a024c06b0e9a658b34b6b5504
DHEERAJ <dheeraj@appscrip.co>
2022-06-04 09:44:11 +0530

    Initial Commit

---
+++ CameraApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraApp
+//
+//  Created by Dheeraj Kumar Sharma on 04/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
